63322345b32f9b7f21e1605698261526f9439f0f
Glitch (thunpitchasongrerk-playlist) <none>
2022-03-09 23:10:41 +0000

    finished!

--- js/script.js
+++ js/script.js
@@ -1,29 +1,7 @@
-// Complete Day 1 goals here
-
-//let songNames = ["moonlight", "happier", "The feels", "STAY"];
-
-//let artists = ["dhruv", "Olivia Rodrigo", "TWICE", "The Kid LAROI"];
-
-//let songImages = [
-//  "https://cdn.smarturl.it/prod/images/retail/upload/caca/ac/l94mwrn3h7dexu3xdn302jzo_450871.jpg",
-//  "https://assets.teenvogue.com/photos/60a7e7547e602be103622f01/2:3/w_800,h_1200,c_limit/unnamed%20(11).jpg",
-// "https://upload.wikimedia.org/wikipedia/en/5/50/Twice_-_The_Feels.png",
-//  "https://upload.wikimedia.org/wikipedia/en/0/0c/The_Kid_Laroi_and_Justin_Bieber_-_Stay.png",
-//];
-
-//let songLinks = [
-//  "https://open.spotify.com/track/2Qn6WHJrY5Im82Jux8FboH?si=c8ee4e0ff53d4d3d",
-//  "https://open.spotify.com/track/2tGvwE8GcFKwNdAXMnlbfl?si=0c56c9725a024946",
-//  "https://open.spotify.com/track/37iQlLWnkc5zx4bnzgNH6x?si=b776a439c202403f",
-//  "https://open.spotify.com/track/5HCyWlXZPP0y6Gqq8TgA20?si=0f193e96a79f40aa",
-//];
-
-//let songLength = ["2:39", "2:56", "3:18", "2:21"];
-
 let moonlight = {
   artist: "dhruv",
   image: "https://cdn.smarturl.it/prod/images/retail/upload/caca/ac/l94mwrn3h7dexu3xdn302jzo_450871.jpg",
-  link: "https://open.spotify.com/track/2Qn6WHJrY5Im82Jux8FboH?si=c8ee4e0ff53d4d3d",
+  link: "https://www.youtube.com/embed/Ob0uXrhdWhk",
   length: "2 minutes and 39 seconds",
   name: "moonlight"
 }
@@ -31,7 +9,7 @@ let moonlight = {
 let happier = {
   artist: "Olivia Rodrigo",
   image: "https://assets.teenvogue.com/photos/60a7e7547e602be103622f01/2:3/w_800,h_1200,c_limit/unnamed%20(11).jpg",
-  link: "https://open.spotify.com/track/2tGvwE8GcFKwNdAXMnlbfl?si=0c56c9725a024946",
+  link: "https://www.youtube.com/embed/ZQFmRXgeR-s",
   length: "2 minutes and 56 seconds",
   name: "happier"
 }
@@ -39,7 +17,7 @@ let happier = {
 let theFeels = {
   artist: "TWICE",
   image: "https://upload.wikimedia.org/wikipedia/en/5/50/Twice_-_The_Feels.png",
-  link: "https://open.spotify.com/track/37iQlLWnkc5zx4bnzgNH6x?si=b776a439c202403f",
+  link: "https://www.youtube.com/embed/f5_wn8mexmM",
   length: "3 minutes and 18 seconds",
   name: "The Feels"
 }
@@ -47,48 +25,26 @@ let theFeels = {
 let stay = {
   artist: "The Kid LAROI",
   image: "https://upload.wikimedia.org/wikipedia/en/0/0c/The_Kid_Laroi_and_Justin_Bieber_-_Stay.png",
-  link: "https://open.spotify.com/track/5HCyWlXZPP0y6Gqq8TgA20?si=0f193e96a79f40aa",
+  link: "https://www.youtube.com/embed/kTJczUoc26U",
   length: "2 minutes and 21 seconds",
   name: "STAY"
 }
 
 
-let playlist = [moonlight, happier, theFeels, stay, addSongs];
+let playlist = [moonlight, happier, theFeels, stay];
 
 
 function displaySongInfo() {
-  // Complete the Day 2 goals inside this function
-  // write your forEach loop here
   playlist.forEach(function (song) {
     $(".songs").append("<p>" + song.name + "</p>");
     $(".artists").append("<p>" + song.artist + "</p>");
     $(".images").append(`<p><img src="${song.image}"></p>`);
-    $(".links").append(`<p><a href="${song.link}">Link</a></p>`);
+    $(".links").append(`<iframe src= ${song.link}></iframe>`);
     $(".lengths").append("<p>" + song.length + "</p>");
   });
-
-//   artists.forEach(function (name) {
-//     $(".artists").append("<p>" + name + "</p>");
-//   });
-
-//   songImages.forEach(function (image) {
-//     $(".images").append(`<p><img src="${image}"></p>`);
-//   });
-
-//   songLinks.forEach(function (link) {
-//     $(".links").append(`<p><a href="${link}">Link</a></p>`);
-//   });
-
-//   songLength.forEach(function (length) {
-//     $(".lengths").append("<p>" + length + "</p>");
-//   });
 }
 
 function emptySongInfo() {
-  /* this function empties the divs each time the button is 
-  clicked so that your playlist does not repeatedly add the data
-  too many times. Try commenting out this function call to see 
-  what happens without it! */
   $(".songs").empty();
   $(".images").empty();
   $(".artists").empty();
@@ -97,27 +53,15 @@ function emptySongInfo() {
 }
 
 function addSongInfo() {
-  // Complete Day 3 goals inside this function
-  let addSongs = {
-  artist: (".artist").val(),
+  let songToAdd = {
+  artist: $(".artist").val(),
   image: $(".image").val(),
   link: $(".link").val(),
   length: $(".length").val(),
-  name: $(".title").val()
-}
-  
-  
-// let song = $(".title").val();
-// playlist.push(song.addSongs); 
-//   let picture = $(".image").val();
-//   songImages.push(picture);
-//   let singer = $(".artist").val();
-//   artists.push(singer);
-//   let link = $(".link").val();
-//   songLinks.push(link);
-//   let length = $(".length").val();
-//   songLength.push(length);
-// }
+  name: $(".title").val(),
+    }
+  playlist.push(songToAdd)
+  }
 
 $(document).ready(function(){
 
